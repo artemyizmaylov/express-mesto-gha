0e1538e27a2c553cd52c7785a8f5c296dd4b79ec
Artemy Izmaylov <artemyizmaylov@gmail.com>
2022-05-05 15:09:09 +0300

    добавлены переменные кодов ошибок

--- middlewares/errorsHandler.js
+++ middlewares/errorsHandler.js
@@ -1,14 +1,18 @@
+const { BAD_REQ_CODE, CONFLICT_CODE, DEFAUTL_CODE } = require('../utils/constants');
+
 module.exports = (err, req, res, next) => {
-  const { statusCode = 500, message } = err;
+  const { statusCode = DEFAUTL_CODE, message } = err;
 
   if (err.code === 11000) {
-    res.status(409).send({ message: 'Email уже зарегистрирован' });
+    res.status(CONFLICT_CODE).send({ message: 'Email уже зарегистрирован' });
   }
 
   if (err.name === 'CastError') {
-    res.status(400).send({ message: 'Неправильный ID' });
+    res.status(BAD_REQ_CODE).send({ message: 'Неправильный ID' });
   }
 
-  res.status(statusCode).send({ message });
+  res.status(statusCode).send(statusCode === DEFAUTL_CODE
+    ? { message: 'На сервере произошла ошибка' }
+    : { message });
   next();
 };
